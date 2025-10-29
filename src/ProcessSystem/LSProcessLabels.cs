8f9fa1d326eedb81f8f095404b9f7467cf7815b8
dranef <dranef.alt@gmail.com>
2025-10-29 09:03:56 -0300

    feat: Add LSProcessLabels class with constants for process management keys

---
+++ src/ProcessSystem/LSProcessLabels.cs
@@ -0,0 +1,8 @@
+namespace LSUtils.ProcessSystem;
+
+public static class LSProcessLabels {
+    public const string MAIN_SEQUENCE_KEY = "mainSequence";
+    public const string MAIN_SELECTOR = "mainSelector";
+    public const string ON_SUCCESS = "onSuccess";
+    public const string ON_FAILURE = "onFailure";
+}
